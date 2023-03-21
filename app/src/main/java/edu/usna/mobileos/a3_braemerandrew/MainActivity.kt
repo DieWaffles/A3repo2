ef3bff8a72308a5601b4db28324f1ff1c799f0b3
DieWaffles <m240576@usna.edum240576@usna.edu>
2023-03-21 08:08:10 -0400

    Initial commit

---
+++ app/src/main/java/edu/usna/mobileos/a3_braemerandrew/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.usna.mobileos.a3_braemerandrew
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
